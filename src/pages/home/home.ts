62cbee8b6cc95552661f0d396f7c71a510cc132f
nyxover <buirette.pierre@gmail.com>
2018-08-18 21:19:01 +0200

    ajout conteur

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,46 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+    searchQuery: string = '';
+    items: string[];
+  
+  public tap: number = 0;
+
   constructor(public navCtrl: NavController) {
 
   }
+
+
+
+  initializeItems() {
+    this.items = [
+      'coinpot',
+      'moondoge',
+      'moonbit'
+    ];
+  }
+
+  getItems(ev: any) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the searchbar
+    const val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+
+
+
+
+  tapEvent(e) {
+    this.tap++
+  }
+
 
 }
